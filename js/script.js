06d8d29b3001d9630eac9cd06e9ef4643205ce7e
Yaroslav Mizin <yareek30@gmail.com>
2022-05-16 15:12:23 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ let images =
     { url: "https://cdn.pixabay.com/photo/2017/01/06/17/28/road-1958388_960_720.jpg"}
 ];
 
+// функция работы слайдера
+
 function initSlider(options) {
     if(!images || !images.length) return;
 
@@ -16,9 +18,12 @@ function initSlider(options) {
         autoplay: true,
       };
 
+// поиск родительских элементов по классам в переменные
     let sliderImages = document.querySelector(".image-slider");
     let sliderButtons = document.querySelector(".buttons");
     let sliderNavigation = document.querySelector(".navigation");
+
+    // запуск функций
 
     initImages();
     initButtons();
@@ -31,35 +36,50 @@ function initSlider(options) {
         initAutoplay();
       }
     
+// декларация внутренних функций
 
+    // добавление массива изображений
     function initImages() {
         images.forEach((image, index) => {
+            // создание дочерних элементов с классом по индексу массива, добавление в дочерний элемент бэкграунда через стиль
             let imageDiv = `<div class="image-slider n${index} ${index === 0 ? "active" : ""}
             "style="background-image:url(${images[index].url});" data-index="${index}"></div>`;
+            // добавление в родительский элемент всех дочерних элементов
             sliderImages.innerHTML += imageDiv;
         });
     }
 
+    // функция работы кнопок
     function initButtons() {
+        // добавление слушателя событий ко всем кнопкам от родительского элемента
         sliderButtons.querySelectorAll(".button").forEach(button => {
             button.addEventListener("click", function () {
+                // поиск слайда с классом "active"
                 let activeSlide = +sliderImages.querySelector(".active").dataset.index;
+                // переменная для аргумента callback функции запуска в конце с условием
                 let nextSlide;
+                // для левой кнопки
                 if (button.classList.contains("left")) {
+                    // первый слайд к последнему
                     nextSlide = activeSlide === 0 ? images.length - 1 : activeSlide - 1;
+                // для правой кнопки - последний слайд к первому
                 } else {
                     nextSlide = activeSlide === images.length - 1 ? 0 : activeSlide + 1;
                 }
+                // запуск функции движения с аргументом от функции кнопок
                 moveSlide(nextSlide);
             });
         });
     }
 
+    // функция точек внизу слайдера
     function initNavigation() {
+        // создание точек с сопряженным к классу атрибутом 
         images.forEach((image, index) => {
             let dot = `<label class="dot-item n${index} ${index === 0 ? "active" : ""}" data-index="${index}"></label>`;
             sliderNavigation.innerHTML += dot;
         });
+        // поиск созданных точек и вызов анонимной функции
         sliderNavigation.querySelectorAll(".dot-item").forEach(dot => {
             dot.addEventListener("click", function () {
                 moveSlide(this.dataset.index);
@@ -67,6 +87,7 @@ function initSlider(options) {
         })
     }
 
+    // функция переключения слайдов + кнопок
     function moveSlide(numb) {
         sliderImages.querySelector(".active").classList.remove("active");
         sliderImages.querySelector(".n" + numb).classList.add("active");
@@ -74,6 +95,7 @@ function initSlider(options) {
         sliderNavigation.querySelector(".n" + numb).classList.add("active");
     }
     
+    // функция автоматического переключения
     function initAutoplay() {
         setInterval(() => {
           activeSlide = +sliderImages.querySelector(".active").dataset.index;
@@ -83,13 +105,14 @@ function initSlider(options) {
       }
 }
 
-
 let slideOptions = {
     dot: true,
     titles: false,
     autoplay: true,
     autoplayInterval: 5000
 }
+
+// запуск функции
 
 document.addEventListener("DOMContentLoaded", function(){
     initSlider(slideOptions);
